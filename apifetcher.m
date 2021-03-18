26037187b49e04a79f2837492728aebe80f399ff
Gaston Sandstig <78793981+GastonSandstig@users.noreply.github.com>
2021-03-18 03:43:32 +0100

    Added comments

--- apifetcher.m
+++ apifetcher.m
@@ -43,11 +43,11 @@ end
 %Check models for PMIDs
 s=0;m=0;
 for i=2:length(models)
-    if 0==isfield(models(i).publication,'link')
+    if 0==isfield(models(i).publication,'link')%Does the metadata contain a link?
         continue
     end
     temp=models(i).publication.link;
-    if isempty(regexp(models(i).publication.link,'pubmed','once'))
+    if isempty(regexp(models(i).publication.link,'pubmed','once'))%Does the link refrence pubmed?
         m=m+1;
         nonPMID_Articles{m}=models(i).publication.link;
         continue%skip non-pubmed articles
@@ -58,6 +58,7 @@ for i=2:length(models)
     end
 end
 
+%API adresses of EPMC, PUBMED, PMC
 api_idconvEPMC=['https://www.ebi.ac.uk/europepmc/webservices/rest/search?'...
     'resultType=idlist&cursorMark=*&pageSize=25&format=json&query=ext_id:'];
 api_idconvPUBMED = 'https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?format=json&ids=';
@@ -65,49 +66,54 @@ api_EPMC_XML='https://www.ebi.ac.uk/europepmc/webservices/rest/';
 api_EPMCcap='/fullTextXML';
 api_PMCpdf = 'https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi?id=';
 
-s=0;m=0;k=0;
+s=0;m=0;k=0;%Where s are the number of EPMC downloads, k for PMC, and m for neither(nonretrivals).
+            %Because k always seems to be zero - Presumably EPMC gets content from PMC - PMC retrival is omitted from fastVer 
 for i=1:length(PMID)
     options=weboptions('Timeout',1e5);
-    url=strjoin([api_idconvEPMC PMID(i)],'');
+    url=strjoin([api_idconvEPMC PMID(i)],'');%Check EPMC for corresponding fulltextID
     queryresult=webread(url,options);
-    if 1==isfield(queryresult.resultList.result,'fullTextIdList') %Dubbelkolla det har, det blev fel nar EPMC var id
+    if 1==isfield(queryresult.resultList.result,'fullTextIdList')
         temp=queryresult.resultList.result.fullTextIdList.fullTextId;
-        options=weboptions('Timeout',1e5,'ContentType','xmldom');
+        options=weboptions('Timeout',1e5,'ContentType','xmldom');%Note that content type is specified
         url=strjoin([api_EPMC_XML temp api_EPMCcap],'');
-        fname=strjoin(['EPMC' string(i) '.xml'],'');
-        try
+        fname=strjoin(['PMID' PMID(i) 'EPMC' string(i) '.xml'],'');%Name of xml-file
+        %CAUTION/BUG! When fname was based on PMID or fulltextID,
+        %some files were "lost" due to overlapping names.        
+        try%downloading the article off EPMC if there is a fulltextID
             queryresult=webread(url,options);
             xmlwrite(fname,queryresult);
             s=s+1;
-            EPMC{s,:}={['EPMC' string(i)] PMID(i)};
             continue
-        catch
+        catch%If EPMC download fails, try PMC instead
             options=weboptions('Timeout',1e5);
-            url=strjoin([api_idconvPUBMED PMID(i)],'');
+            url=strjoin([api_idconvPUBMED PMID(i)],'');%Check for PMCID
             queryresult=webread(url,options);
-            pause(.33)%Not tax pubmed?
+            pause(.34)%Limit of requests per second on pubmed is 3
             if isfield(queryresult.records,'pmcid')==0
                 m=m+1;
-                nonPMCID(m)=PMID(i);
+                nonPMCID(m)=PMID(i);%List of PMIDs without PMCID
             else
-                try
+                try%downloading the article off PMC if there is a PMCID
                     options=weboptions('Timeout',1e5,'ContentType','xmldom');
                     url=strjoin([api_PMCpdf queryresult.records.pmcid],'');
                     queryresult=webread(url,options);
                     xmlwrite('temp.xml',queryresult);
+                    %Possible improvement: readstruct did not seem to work
+                    %directly on the url, so the xml is first written down
+                    %and then readstruct:ed. Going directly to struct would
+                    %of course be preferable.
                     queryresult=readstruct('temp.xml');
-                    tarurl=queryresult.records.record.link.hrefAttribute;
+                    tarurl=queryresult.records.record.link.hrefAttribute;%Retrive link to tar.gz 
                     gunzip(tarurl,'temp');
                     untar(['temp/' PMID(i) '.tar'],'Articles');
                     k=k+1;
-                    PMCID{k,:}={queryresult.records.pmcid PMID(i)};
                 catch
                     m=m+1;
                     nonPMCID(m)=PMID(i);
                 end
             end
         end
-    else
+    else %Identical to "If epmc download fails"
         options=weboptions('Timeout',1e5);
         url=strjoin([api_idconvPUBMED PMID(i)],'');
         queryresult=webread(url,options);
