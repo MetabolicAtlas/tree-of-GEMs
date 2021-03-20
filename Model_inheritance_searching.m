58a436a81ff3706dd4225a7898feffd05f3a5601
Gaston Sandstig <78793981+GastonSandstig@users.noreply.github.com>
2021-03-20 09:15:27 +0100

    Fixed minor bugs, reordered a bit for readability and easy collapsing of sections

--- Model_inheritance_searching.m
+++ Model_inheritance_searching.m
@@ -3,7 +3,7 @@ clc
 % addpath 'D:\GEMS\Function folder testing'
 load('Model_hits.mat', 'Model_hits');
 load('cura_biomodels_metadata.mat');
-
+%% Currently unused
 % myFolder = 'D:\GEMS\Extracted_new_publications';
 %
 % filePattern = fullfile(myFolder, '*.pdf');
@@ -15,14 +15,18 @@ load('cura_biomodels_metadata.mat');
 %
 % pdf=theFiles(i,1).name
 % open(pdf);
-
-string_nr='string_nr';  file_nr='File_nr';  string_of_text_found='string_of_text_found';    full_title='full_title';
-empty_num = {}; empty_char = {{}};
+%% Construct table of information to display
+string_nr='string_nr';
+file_nr='File_nr';
+string_of_text_found='string_of_text_found';
+full_title='full_title';
+empty_num = {};
+empty_char = {{}};
 display=struct(string_nr,empty_num, string_of_text_found,empty_char, full_title,empty_char);
-
+%% Assign values to and display table
 j=0;
 p=0;
-for j=1:24
+for j=1:length(Model_hits)
     if Model_hits(j).Found_in_publication_title==i
         % hits=Model_hits(j);
         
@@ -32,32 +36,22 @@ for j=1:24
         display(p).string_of_text_found=Model_hits(j).String;
         display(p).full_title=Model_hits(j).String_publication_title;
         
-    end
-    
+    end 
 end
 
 t=struct2table(display)
-
+%% Open article webpage
 j=find([Model_hits.Found_in_publication_title]==i);
-k=j(1);
+k=j(1);%j contains identical 
 PMID=Model_hits(k).Found_in_file(1:8);
 webpage=['http://identifiers.org/pubmed/' PMID];
 web(webpage);
-
-m=0;
-while m==0
-    prompt = 'Coppy string nr to clipboard ';
-    a = input(prompt);
-    
-    str = input(prompt,'s');
-    if isempty(str)
+%% User clipboard prompt
+while 0==0 %Endless while loop
+    prompt = 'Choose string number to copy:';
+    str_nr = input(prompt);
+    if isempty(str_nr)
         break
     end
-    
-    clipboard('copy', display(a).string_of_text_found)
+    clipboard('copy', display(str_nr).string_of_text_found)
 end
-end
-
-
-
-
