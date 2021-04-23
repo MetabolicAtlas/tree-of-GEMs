f0c16e1ded555bbb9632812a43c529012cc3fabb
Gaston Sandstig <sandstig@student.chalmers.se>
2021-04-23 23:44:43 +0200

    Minor fixes

--- connMapper.m
+++ connMapper.m
@@ -3,15 +3,14 @@ if isfile('dataset.mat')==0
     json_packer
 end
 load('dataset.mat')
-connMatrix = [];
-for i = 1:length(dataset)
+numModels = length(dataset);
+connMatrix = zeros(numModels,numModels);
+for i = 1:numModels
     if ~isempty(dataset(i).connection)
         for j = 1:length(dataset(i).connection)
-            for k = 1:length(dataset)
+            for k = 1:numModels
                 if strcmpi(dataset(k).articleInformation.PMID,dataset(i).connection(j).PMID)&&j~=k
                     connMatrix(i,k) = 1;
-                else
-                    connMatrix(i,k) = 0;
                 end
             end
         end
